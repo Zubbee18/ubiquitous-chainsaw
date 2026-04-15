8199ce3752f0432e910970a532de65248d206251
Deborah Anyachukwu <108271834+Zubbee18@users.noreply.github.com>
2026-04-15 15:15:07 +0100

    Update: included cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+import cors from 'cors'
 import express from "express"
 import { classifyRouter } from './Routes/appRouter.js'
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
+app.use(cors())
 
 app.use('/api', classifyRouter)
 
