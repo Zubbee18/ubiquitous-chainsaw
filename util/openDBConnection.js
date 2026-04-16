928dde3b5ad3cc1eea4bf1ea8793d35f50edaab0
Deborah Anyachukwu <108271834+Zubbee18@users.noreply.github.com>
2026-04-16 20:03:11 +0100

    Switch to better-sqlite3 for better deployment compatibility

--- util/openDBConnection.js
+++ util/openDBConnection.js
@@ -1,12 +1,30 @@
-import sqlite from 'sqlite3'
-import { open } from 'sqlite'
+import Database from 'better-sqlite3'
 import path from 'path'
 
-export async function openDatabaseConnection() {
-    const db = await open({
-        filename: path.join('database.db'),
-        driver: sqlite.Database
-    })
+// Wrapper to provide async-like interface for better-sqlite3
+class AsyncDatabase {
+    constructor(db) {
+        this.db = db
+    }
 
-    return db
+    async get(sql, params = []) {
+        return this.db.prepare(sql).get(...params)
+    }
+
+    async all(sql, params = []) {
+        return this.db.prepare(sql).all(...params)
+    }
+
+    async run(sql, params = []) {
+        return this.db.prepare(sql).run(...params)
+    }
+
+    async exec(sql) {
+        return this.db.exec(sql)
+    }
+}
+
+export async function openDatabaseConnection() {
+    const db = new Database(path.join('database.db'))
+    return new AsyncDatabase(db)
 }
