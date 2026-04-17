a26109488fb603e5fd84403f3db2672cff89573b
Deborah Anyachukwu <108271834+Zubbee18@users.noreply.github.com>
2026-04-17 23:11:19 +0100

    Reverse update createTable.js

--- util/createTable.js
+++ util/createTable.js
@@ -1,40 +1,21 @@
-import Database from 'better-sqlite3'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import { openDatabaseConnection } from "./openDBConnection.js"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+export function createTable() {
+    const db = openDatabaseConnection()
+export async function createTable() {
+    const db = await openDatabaseConnection()
 
-// Wrapper to provide async-like interface for better-sqlite3
-class AsyncDatabase {
-    constructor(db) {
-        this.db = db
-    }
+    db.exec(`
+    await db.exec(`
+CREATE TABLE IF NOT EXISTS profiles (
+    id BLOB PRIMARY KEY,
+    name TEXT NOT NULL,
+@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS profiles (
+)
+`)
 
-    async get(sql, params = []) {
-        return this.db.prepare(sql).get(...params)
-    }
+    db.close()
+    await db.close()
+    console.log('Profiles table has been created')
 
-    async all(sql, params = []) {
-        return this.db.prepare(sql).all(...params)
-    }
-
-    async run(sql, params = []) {
-        const result = this.db.prepare(sql).run(...params)
-        return result
-    }
-
-    async exec(sql) {
-        return this.db.exec(sql)
-    }
-
-    async close() {
-        return this.db.close()
-    }
-}
-
-export async function openDatabaseConnection() {
-    const dbPath = process.env.DB_PATH || path.join(__dirname, '..', 'database.db')
-    const db = new Database(dbPath)
-    return new AsyncDatabase(db)
 }
