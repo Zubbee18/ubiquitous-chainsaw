970803e4a85d375d585ea64b05194804b09474ab
Deborah Anyachukwu <108271834+Zubbee18@users.noreply.github.com>
2026-04-22 05:23:23 +0100

    fix: reorder routes in profilesRouter for endpoint specificity

--- routes/profilesRouter.js
+++ routes/profilesRouter.js
@@ -10,13 +10,13 @@ export const profilesRouter = express.Router()
 // for the post endpoint of /api/profiles
 profilesRouter.post('/', handlePostProfiles)
 
-// for get endpoint of api/profiles/{id}
-profilesRouter.get('/:id', handleGetProfilesById) // come back to id later
-
+// for get endpoint of /api/profiles for natural language query
 profilesRouter.get('/search', handleGetProfilesBySearchQueryParams)
+
+// for get endpoint of api/profiles/{id}
+profilesRouter.get('/:id', handleGetProfilesById)
 
 // for get endpoing of api/profiles
 profilesRouter.get('/', handleGetProfilesByQueryParams)
-
 
 profilesRouter.delete('/:id', handleDeleteProfilesById)
