2a336ec796df52e65e4921620bebd1e9bf8ca8c1
Deborah Anyachukwu <108271834+Zubbee18@users.noreply.github.com>
2026-04-29 20:39:20 +0100

    fix(userData.js): include logic to assign admin role

--- models/userData.js
+++ models/userData.js
@@ -1,46 +1,56 @@
-import { v7 as uuidv7 } from 'uuid'
-import { db } from '../db/openDBConnection.js'
+import { v7 as uuidv7 } from "uuid";
+import { db } from "../db/openDBConnection.js";
 
 export async function checkUserExists(userId, github = true) {
+  console.log(github, userId);
+  const userExists = await db.query(
+    `SELECT * FROM users WHERE ${github ? "github_id" : "id"}=$1`,
+    [userId],
+  );
 
-    console.log(github, userId)
-    const userExists = await db.query(`SELECT * FROM users WHERE ${github? 'github_id': 'id'}=$1`, [userId])
+  let existing = false;
 
-    let existing = false
+  if (userExists.rows[0]) existing = true;
 
-    if (userExists.rows[0]) existing = true
-    
-    return existing
+  return existing;
 }
 
-
 export async function getLoginUserFromId(githubUserId) {
+  const result = await db.query(
+    `UPDATE users SET last_login_at = NOW() AT TIME ZONE 'UTC', is_active = TRUE WHERE github_id=$1
+                    RETURNING *`,
+    [githubUserId],
+  );
 
-    const result = await db.query(`UPDATE users SET last_login_at = NOW() AT TIME ZONE 'UTC', is_active = TRUE WHERE github_id=$1
-                    RETURNING *`, [githubUserId])
-
-    return result.rows[0]
+  return result.rows[0];
 }
 
 export async function getUser(userId) {
+  const user = await db.query("SELECT * FROM users WHERE id=$1", [userId]);
 
-    const user = await db.query('SELECT * FROM users WHERE id=$1', [userId])
-    
-    return user.rows[0]
+  return user.rows[0];
 }
 
 export async function createAndLoginUser(login, id, avatar_url, email) {
+  const userId = uuidv7();
 
-    const userId = uuidv7()
+  const adminCheck = await db.query(
+    `SELECT 1 FROM users WHERE role = 'admin' LIMIT 1`,
+  );
+  const role = adminCheck.rows.length > 0 ? "analyst" : "admin";
 
-    const newUser = await db.query(`INSERT INTO users (id, github_id, username, email, avatar_url, role, is_active, last_login_at)
+  const newUser = await db.query(
+    `INSERT INTO users (id, github_id, username, email, avatar_url, role, is_active, last_login_at)
                     VALUES ($1, $2, $3, $4, $5, $6, $7, NOW() AT TIME ZONE 'UTC')
-                    RETURNING *`, [userId, id, login, email, avatar_url, 'analyst', true])
+                    RETURNING *`,
+    [userId, id, login, email, avatar_url, role, true],
+  );
 
-    return newUser.rows[0]
+  return newUser.rows[0];
 }
 
 export async function logoutUserDB(userId) {
-
-    return await db.query(`UPDATE users SET is_active = FALSE WHERE id=$1`, [userId])
+  return await db.query(`UPDATE users SET is_active = FALSE WHERE id=$1`, [
+    userId,
+  ]);
 }
